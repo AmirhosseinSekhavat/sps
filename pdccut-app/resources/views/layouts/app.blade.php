4961dd3359d77dd5cfb5b5fcb0c873f91416ac26
AmirhosseinSekhavat <amirhossein.sekhavat@gmail.com>
2025-08-19 23:31:53 +0330

    perf(fonts): add Medium preload and inline @font-face with font-display:block to reduce FOUT

--- pdccut-app/resources/views/layouts/app.blade.php
+++ pdccut-app/resources/views/layouts/app.blade.php
@@ -18,6 +18,31 @@
     <!-- Preload critical Farsi fonts -->
     <link rel="preload" as="font" type="font/woff2" href="{{ asset('fonts/Farsi numerals/Webfonts/fonts/Woff2/IRANSansXFaNum-Regular.woff2') }}" crossorigin>
     <link rel="preload" as="font" type="font/woff2" href="{{ asset('fonts/Farsi numerals/Webfonts/fonts/Woff2/IRANSansXFaNum-Bold.woff2') }}" crossorigin>
+    <link rel="preload" as="font" type="font/woff2" href="{{ asset('fonts/Farsi numerals/Webfonts/fonts/Woff2/IRANSansXFaNum-Medium.woff2') }}" crossorigin>
+
+    <style>
+        @font-face {
+            font-family: 'IRANSansX';
+            src: url('{{ asset('fonts/Farsi numerals/Webfonts/fonts/Woff2/IRANSansXFaNum-Regular.woff2') }}') format('woff2');
+            font-weight: 400;
+            font-style: normal;
+            font-display: block;
+        }
+        @font-face {
+            font-family: 'IRANSansX';
+            src: url('{{ asset('fonts/Farsi numerals/Webfonts/fonts/Woff2/IRANSansXFaNum-Medium.woff2') }}') format('woff2');
+            font-weight: 500;
+            font-style: normal;
+            font-display: block;
+        }
+        @font-face {
+            font-family: 'IRANSansX';
+            src: url('{{ asset('fonts/Farsi numerals/Webfonts/fonts/Woff2/IRANSansXFaNum-Bold.woff2') }}') format('woff2');
+            font-weight: 700;
+            font-style: normal;
+            font-display: block;
+        }
+    </style>
     
     <!-- Load our custom CSS last -->
     @vite(['resources/css/app.css'])
